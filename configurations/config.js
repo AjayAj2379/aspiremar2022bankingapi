876a6582151f334134ab51d5ef426d1b7225eb5c
eswaribala <parameswaribala@gmail.com>
2022-03-30 08:31:00 +0530

    updated

--- configurations/config.js
+++ configurations/config.js
@@ -1,9 +1,9 @@
 exports.port=4000
-exports.user='eswari'
-exports.password='test@123'
-exports. authdb='admin'
+//exports.user='eswari'
+//exports.password='test@123'
+//exports. authdb='admin'
 exports.url='127.0.0.1'
-exports.mongodb='happytripdb'
+exports.mongodb='bankingdb'
 exports.mongoport=27017
 
 
--- controllers/bankingController.js
+++ controllers/bankingController.js
@@ -1,13 +1,24 @@
-const https = require("https");
 const fs = require("fs");
 express=require('express')
 cors=require('cors')
 bodyParser=require('body-parser')
 config=require('../configurations/config')
 app=new express()
+app.use(cors());
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+//http get
 app.get("/",function(request,response){
     response.send("Testing Express ....!");
 });
+app.post("/customers",function(request,response){
+    console.log(request.body);
+    response.send({message:"Data Sored....."});
+})
+
+
+
+
 
 app.set('port',config.port);
 
@@ -15,3 +26,6 @@ app.listen(app.get('port'),function()
 {
     console.log('Server started at ....'+app.get('port'));
 });
+
+
+
