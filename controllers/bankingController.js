b23d39d5b9c0f2652929dac0d034d6965ce3caea
eswaribala <parameswaribala@gmail.com>
2022-03-30 07:30:37 +0530

    updated

--- controllers/bankingController.js
+++ controllers/bankingController.js
@@ -1,8 +1,17 @@
+const https = require("https");
+const fs = require("fs");
 express=require('express')
 cors=require('cors')
 bodyParser=require('body-parser')
-
+config=require('../configurations/config')
 app=new express()
 app.get("/",function(request,response){
     response.send("Testing Express ....!");
+});
+
+app.set('port',config.port);
+
+app.listen(app.get('port'),function()
+{
+    console.log('Server started at ....'+app.get('port'));
 });
