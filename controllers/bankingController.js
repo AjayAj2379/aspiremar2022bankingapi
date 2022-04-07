d3dd63bc4b3f1daa3977b787b4eb8f43832aefcb
eswaribala <parameswaribala@gmail.com>
2022-04-07 08:01:56 +0530

    updated

--- controllers/bankingController.js
+++ controllers/bankingController.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 express=require('express')
+var multer  =   require('multer');
 cors=require('cors')
 bodyParser=require('body-parser')
 config=require('../configurations/config')
@@ -36,6 +37,65 @@ query=customerService.Search(request.query.userName);
     });
 
 });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+
+        // Uploads is the Upload_folder_name
+        cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + "-" + Date.now()+".jpg")
+    }
+})
+
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 1 * 10000 * 10000;
+
+var upload = multer({
+    storage: storage,
+    limits: { fileSize: maxSize },
+    fileFilter: function (req, file, cb){
+
+        // Set the filetypes, it is optional
+        var filetypes = /jpeg|jpg|png/;
+        var mimetype = filetypes.test(file.mimetype);
+
+        var extname = filetypes.test(path.extname(
+            file.originalname).toLowerCase());
+
+        if (mimetype && extname) {
+            return cb(null, true);
+        }
+
+        cb("Error: File upload only supports the "
+            + "following filetypes - " + filetypes);
+    }
+
+// mypic is the name of file attribute
+}).single("mypic");
+
+app.post('/upload-file',function(req,res){
+    // Error MiddleWare for multer file upload, so if any
+    // error occurs, the image would not be uploaded!
+    upload(req,res,function(err) {
+
+        if(err) {
+
+            // ERROR occured (here it can be occured due
+            // to uploading image of size greater than
+            // 1MB or uploading different file type)
+            res.send(err)
+        }
+        else {
+
+            // SUCCESS, image successfully uploaded
+            res.send("Success, Image uploaded!")
+        }
+    })
+});
+
 
 
 
